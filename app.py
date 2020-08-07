fb9d47941fbba784fee5e7b1d680c21bf05e4a40
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-07 22:05:32 +0300

    Create template

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask
 from config import Configuration
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 
+
 @app.route('/')
+@app.route('/index')
 def index():
-    return 'Hello world'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 class Configuration():
-    DEBUG = True
+    debug = True
