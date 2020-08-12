38defc5724a0be17693ec1007f89fb020325a580
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-12 20:27:13 +0300

    Registrate blueprint

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+from flask.helpers import url_for
 from config import Configuration
+from posts.blueprint import posts
 
 from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
+app.register_blueprint(posts, url_prefix='/blog')
 
 
 @app.route('/')
