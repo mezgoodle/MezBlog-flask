9129c774658dc492bd1891f6d0c3410904bbf773
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-13 21:00:56 +0300

    Create config for db

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ from config import Configuration
 from posts.blueprint import posts
 
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 app.register_blueprint(posts, url_prefix='/blog')
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 class Configuration():
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
