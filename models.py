f0c6d1e30d291a0a1e2f7376c3c85e56cd94dbad
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-18 12:42:34 +0300

    Add new model Tag

--- models.py
+++ models.py
@@ -24,3 +24,16 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'<Post id: {self.id}, title: {self.title}'
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    slug = db.Column(db.String(100), unique=True)
+
+    def __init__(self, *args, **kwargs):
+        super(Tag, self).__init__(*args, **kwargs)
+        self.slug = slugify(self.name)
+
+    def __repr__(self):
+        return f'<Tage id: {self.id}, name: {self.name}'
