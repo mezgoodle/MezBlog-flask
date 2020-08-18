c415bb712e3b61e58a6e8a650e26fd79f508c58a
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-18 13:56:45 +0300

    Render tags

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -15,3 +15,11 @@ def post_detail(slug):
     post = Post.query.filter(Post.slug==slug).first()
     tags = post.tags
     return render_template('posts/post_detail.html', post=post, tags=tags)
+
+@posts.route('/tag/<slug>')
+def tag_detail(slug):
+    tag = Tag.query.filter(Tag.slug==slug).first()
+    posts = tag.posts.all()
+    return render_template('posts/tag_detail.html', tag=tag, posts=posts)
+
+
