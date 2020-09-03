0a23d987e1561331e5e015578762450d70aa6a28
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-09-03 08:46:08 +0300

    Update blueprint.py

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -4,10 +4,12 @@ from .forms import PostForm
 from app import db
 
 from flask import Blueprint, render_template, request, redirect, url_for
+from flask_security import login_required
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
 @posts.route('/create', methods=['GET', 'POST'])
+@login_required
 def create_post():
     if request.method == 'POST':
         title = request.form('title')
@@ -24,6 +26,7 @@ def create_post():
 
 
 @posts.route('/<slug>/edit/', methods=['POST', 'GET'])
+@login_required
 def edit_post(slug):
     post = Post.query.filter(Post.slug==slug).first()
     if request.method == 'POST':
