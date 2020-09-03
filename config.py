bb9fd397697aeb7c83813efd78eda7c53cc0fef0
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-09-03 08:48:34 +0300

    Set salt

--- config.py
+++ config.py
@@ -3,3 +3,7 @@ class Configuration():
     SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'secret_key'
+
+    ### Flask Security
+    SECURITY_PASSWORD_SALT = 'salt'
+    SECURITY_PASSWORD_HASH = 'sha512_crypt'
